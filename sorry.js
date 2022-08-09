5d47e85b1d377dd5c2aa959eee666b8e3b825d88
Yu Kakinoki <yukakinoki@gmail.com>
2022-08-09 11:54:54 +0100

    commit photos

--- sorry.js
+++ sorry.js
@@ -1,13 +1,60 @@
-function getRandomSize(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+let slides = document.querySelectorAll('.slide');
+let thumbnails = document.querySelectorAll('.thumbnail');
+let currentSlide = document.querySelector('.slide.show');
+let slideCount = slides.length;
+let currentSlideId = currentSlide.dataset.slide;
+let nextSlideBtn = document.querySelector('.slide-btn.next');
+let prevSlideBtn = document.querySelector('.slide-btn.prev');
+let nextSlideTimer = 1000;
+
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', showSlide);
+});
+
+nextSlideBtn.addEventListener('click', nextSlide);
+prevSlideBtn.addEventListener('click', prevSlide);
+
+let slideshow = setInterval(nextSlide, nextSlideTimer);
+
+function showSlide(e) {
+    let slideId = e.currentTarget.dataset.slide;
+
+    currentSlide.classList.remove('show');
+
+    currentSlide = document.querySelector(`.slide[data-slide="${slideId}"`);
+
+    currentSlide.classList.add('show');
+
+    resetSlideShow();
 }
 
-var allImages = "";
+function nextSlide() {
+    let nextSlideId = currentSlideId >= slideCount ? 1 : parseInt(currentSlideId) + 1;
 
-for (var i = 0; i < 25; i++) {
-  var width = getRandomSize(200, 400);
-  var height =  getRandomSize(200, 400);
-  allImages += '<img src="/uthanized/screens/" alt="pretty kitty">';
+    currentSlide.classList.remove('show');
+
+    currentSlide = document.querySelector(`.slide[data-slide="${nextSlideId}"`);
+    currentSlideId = currentSlide.dataset.slide;
+
+    currentSlide.classList.add('show');
+
+    resetSlideShow();
 }
 
-$('#photos').append(allImages);
+function prevSlide() {
+    let prevSlideId = currentSlideId <= 1 ? slideCount : parseInt(currentSlideId) - 1;
+
+    currentSlide.classList.remove('show');
+
+    currentSlide = document.querySelector(`.slide[data-slide="${prevSlideId}"`);
+    currentSlideId = currentSlide.dataset.slide;
+
+    currentSlide.classList.add('show');
+
+    resetSlideShow();
+}
+
+function resetSlideShow() {
+    clearInterval(slideshow);
+    slideshow = setInterval(nextSlide, nextSlideTimer);
+}
