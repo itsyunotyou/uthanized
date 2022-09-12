919effff25438b9ce849b6c8d0b0ad0c07a3977e
Yu Kakinoki <yukakinoki@gmail.com>
2022-09-12 22:12:20 +0100

    Edited danger.html

---
+++ danger.js
@@ -0,0 +1,152 @@
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup1").style.display = "block";
+        },
+        1000
+    )
+});
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup1").style.display = "none";
+});
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup2").style.display = "block";
+        },
+        1200
+    )
+});
+document.querySelector("#close2").addEventListener("click", function(){
+    document.querySelector(".popup2").style.display = "none";
+});
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup3").style.display = "block";
+        },
+        1500
+    )
+});
+document.querySelector("#close3").addEventListener("click", function(){
+    document.querySelector(".popup3").style.display = "none";
+});
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup4").style.display = "block";
+        },
+        1500
+    )
+});
+document.querySelector("#close4").addEventListener("click", function(){
+    document.querySelector(".popup4").style.display = "none";
+});
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup5").style.display = "block";
+        },
+        1100
+    )
+});
+document.querySelector("#close5").addEventListener("click", function(){
+    document.querySelector(".popup5").style.display = "none";
+});
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup6").style.display = "block";
+        },
+        1470
+    )
+});
+document.querySelector("#close6").addEventListener("click", function(){
+    document.querySelector(".popup6").style.display = "none";
+});
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup7").style.display = "block";
+        },
+        1470
+    )
+});
+document.querySelector("#close7").addEventListener("click", function(){
+    document.querySelector(".popup7").style.display = "none";
+});
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup8").style.display = "block";
+        },
+        1470
+    )
+});
+document.querySelector("#close8").addEventListener("click", function(){
+    document.querySelector(".popup8").style.display = "none";
+});
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup9").style.display = "block";
+        },
+        1470
+    )
+});
+document.querySelector("#close9").addEventListener("click", function(){
+    document.querySelector(".popup9").style.display = "none";
+});
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup10").style.display = "block";
+        },
+        1470
+    )
+});
+document.querySelector("#close10").addEventListener("click", function(){
+    document.querySelector(".popup10").style.display = "none";
+});
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup11").style.display = "block";
+        },
+        1470
+    )
+});
+document.querySelector("#close11").addEventListener("click", function(){
+    document.querySelector(".popup11").style.display = "none";
+});
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup12").style.display = "block";
+        },
+        1470
+    )
+});
+document.querySelector("#close12").addEventListener("click", function(){
+    document.querySelector(".popup12").style.display = "none";
+});
