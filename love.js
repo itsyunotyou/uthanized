6c566eb8b08ce28515f864414175069ac3bec030
Yu Kakinoki <yukakinoki@gmail.com>
2023-02-01 21:56:20 +0000

    Working on flower code

--- love.js
+++ love.js
@@ -1,14 +1,16 @@
-const { div } = require("prelude-ls");
+let lovesMeState = true;
+
 
 const petalNumbers = 24;
 const flower = document.getElementById("flower");
-for (var i = 0; i < petalNumbers; i++) {
-  flower.innerHTML += '
-  <div class = petal-container-wrapper style="transform: rotate(45)">
-  <div class = peetal-container>
-  <div class=petal> </div>
+for (var i = 0; i < petalNumbers; i++){
+  const rotate = i /petalNumbers * 360;
+  flower.innerHTML += '<div class="petal-container-wrapper" style="transform: rotate('+rotate+'deg"><div class="petal-container"><div class="petal" id = "petal-'+i+'" onClick="toggleLovesMe('+i+')"></div></div></div>'
+} 
 
-  </div>
-  </div>'
 
+window. toggleLovesMe = (petalKey)=>{
+  lovesMeState = !lovesMeState;
+  console.log("Toggle Love", petalKey);
+  document.getElementById("lovesMe").innerHTML = "She   Loves Me" + (lovesMeState ? "."  : "  Not.")
 }
